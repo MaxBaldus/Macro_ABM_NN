b243961480c444a69d293bf31e0d0c1b07d4117e
MaxBaldus <maxbaldus@gmx.net>
2023-02-17 17:01:50 +0100

    BAM clean

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from models.toymodel import Toymodel # importing toy model (benchmark?)
 from models.BAM import BAM_base # BAM base model
+from models.BAM_nonrobust import BAM_base_nonrobust
 #from models.BAM_firstcycles import BAM_base # BAM base model
 from estimation.data_prep import Filters
 
@@ -20,15 +21,22 @@ from estimation.data_prep import Filters
 #################################################################################################
 
 """ intial Parameters to be estimated later"""
-parameters = {'Nh':500, 'Nf':100, 'Nb':10, 'T':100  , 
+parameters = {'Nh':500, 'Nf':100, 'Nb':10, 'T':100, 
               'Z':2, 'M':4, 'H':2, 'H_eta': 0.1, 'H_rho':0.1, 'H_phi':0.01, 'h_xi':0.05,
               'c_P':1, 'c_R':0.5}
 # 'Z':2 im Buch -> bei ihm 3 !!!
+BAM_old = BAM_base_nonrobust(MC = 1, parameters=parameters, plots=True)
+BAM_simulation = BAM_old.simulation()
+print(BAM_simulation)
 
 """ simulate the base model"""
-BAM = BAM_base(MC = 1, parameters=parameters, plots=True) 
+BAM = BAM_base(T=10, MC = 1, plots=True,
+               Nh=500, Nf=100, Nb=10,
+               H_eta=0.1, H_rho=0.1, H_phi=0.01, h_xi=0.05
+               ) 
 BAM_simulation = BAM.simulation()
 print(BAM_simulation)
+
 
 #################################################################################################
 # Estimating the simple macro ABM
